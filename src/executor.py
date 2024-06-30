106b57e1152c2fd0b44ea4e36887d313ebc36fad
AbhinavDevelops <abhinav.rajaram02@gmail.com>
2024-06-30 16:22:42 +0800

    add docker compose file

--- src/executor.py
+++ src/executor.py
@@ -1,31 +1,43 @@
 import csv
 import subprocess
+import os
+from supabase import create_client, Client
 
 
-def execute_script(input_data):
-    process = subprocess.Popen(['python', 'script.py', input_data],
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE,
-                               universal_newlines=True)
-    output, error = process.communicate()
-    return (output)
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+response = supabase.table("test_cases").select("*").execute()
+
+print(response.data)
 
 
-# Opening and reading the CSV file
-with open('test_cases.csv', 'r') as file:
-    csv_reader = csv.reader(file)
-    next(csv_reader)  # skipping the header
+# def execute_script(input_data):
+#     process = subprocess.Popen(['python', 'script.py', input_data],
+#                                stdout=subprocess.PIPE,
+#                                stderr=subprocess.PIPE,
+#                                universal_newlines=True)
+#     output, error = process.communicate()
+#     return (output)
 
-    for row in csv_reader:
-        input_data = row[0]
-        expected_output = (row[1])
-        actual_output = execute_script(input_data)
 
-        print(actual_output)
+# # Opening and reading the CSV file
+# with open('test_cases.csv', 'r') as file:
+#     csv_reader = csv.reader(file)
+#     next(csv_reader)  # skipping the header
 
-        if actual_output == expected_output:
-            print(
-                f"Test Passed: Input {input_data} |  Expected Output: {expected_output} | Actual Output: {actual_output}")
-        else:
-            print(
-                f"Test Failed: Input {input_data} |  Expected Output: {expected_output} | Actual Output: {actual_output}")
+#     for row in csv_reader:
+#         input_data = row[0]
+#         expected_output = (row[1])
+#         actual_output = execute_script(input_data)
+
+#         print(actual_output)
+
+#         if actual_output == expected_output:
+#             print(
+#                 f"Test Passed: Input {input_data} |  Expected Output: {expected_output} | Actual Output: {actual_output}")
+#         else:
+#             print(
+#                 f"Test Failed: Input {input_data} |  Expected Output: {expected_output} | Actual Output: {actual_output}")
